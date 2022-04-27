2e0e69f19a6735d0b42a54500587ba9daddae89b
ydzhou <yd.zhou@me.com>
2022-04-27 04:36:34 -0700

    Update picom

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -4,12 +4,15 @@ WALLPAPER=~/Pictures/wp/xb2wp04.jpg
 
 imwheel &
 
-# picom -b --config ~/dwmconfigs/compton.conf &
+picom -b --config ~/dwmconfigs/compton.conf &
 
 # xfce4-panel --disable-wm-check &
-
-./statusbar.sh &
 
 feh --bg-fill ${WALLPAPER} &
 
 ibus-daemon -drxR &
+
+while true; do
+    xsetroot -name "$(date +'%B%d %A %I:%M')"
+    sleep 2
+done
--- dwm/statusbar.sh
+++
@@ -1,4 +0,0 @@
-while true; do
-    xsetroot -name "$(date +'%B%d %A %I:%M')"
-    sleep 2
-done
